5ee519be296f937fb5012ae280af9d12941d723d
Mohamed Hamza <mohamedelwan260@gmail.com>
2025-09-23 03:24:27 +0300

    1

--- ass.1/ass.1.cpp
+++ ass.1/ass.1.cpp
@@ -1,11 +1,11 @@
 #include "image_class.h"
 #include <bits/stdc++.h>
 using namespace std;
-void BlackAndWhite(Image &image)
+void GrayScale(Image &image)
 {
     for (int i = 0; i < image.width; i++)
     {
-        for (int j = 0; j < image.height; j++) // get avg of every 3 pixels and equate them to it
+        for (int j = 0; j < image.height; j++) // get avg of every 3 leds and equate the pixel to it
         {
             int avg = 0;
             for (int k = 0; k < 3; k++)
@@ -20,11 +20,36 @@ void BlackAndWhite(Image &image)
         }
     }
 }
+void BlackAndWhite(Image &image)
+{
+    for (int i = 0; i < image.width; i++)
+    {
+        for (int j = 0; j < image.height; j++) // get avg of every 3 leds and equate the pixel to it
+        {
+            int avg = 0;
+            for (int k = 0; k < 3; k++)
+            {
+                avg += image(i, j, k);
+            }
+            avg /= 3;
+            for (int k = 0; k < 3; k++)
+            {
+                if (avg <= 255 / 2)
+                {
+                    image(i, j, k) = 0;
+                }
+                else
+                {
+                    image(i, j, k) = 255;
+                }
+            }
+        }
+    }
+}
 int GetChoice()
 {
     int choice;
     vector<string> choices = {"1-Black-And-White filter", "2-", "3-", "4-"};
-
     cout << "Choose" << endl;
     for (int i = 0; i < choices.size(); i++)
     {
@@ -36,10 +61,17 @@ int GetChoice()
 int main()
 {
     Image usedImage;
-    usedImage.loadNewImage("F:/programming/oop/ass.1/KedaElGenabTmam.png");
+    string userInput;
+    cout << "Input image" << endl;
+    cin >> userInput;
+    usedImage.loadNewImage(userInput);
     switch (GetChoice())
     {
     case 1:
+        GrayScale(usedImage);
+        usedImage.saveImage("NewImage.png");
+        break;
+    case 2:
         BlackAndWhite(usedImage);
         usedImage.saveImage("NewImage.png");
         break;
