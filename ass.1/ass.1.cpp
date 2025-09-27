d5b575f56c417f908364f04f45aafcb2fcca45e8
Mohamed Hamza <mohamedelwan260@gmail.com>
2025-09-27 19:35:17 +0300

    up

--- ass.1/ass.1.cpp
+++ ass.1/ass.1.cpp
@@ -68,7 +68,7 @@ void FlipImageHorizontally(Image &image)
 }
 void FlipImageVertically(Image &image)
 {
-    int y = image.height-1;
+    int y = image.height - 1;
     Image emptyImage(image.width, image.height); // creating an empty image
     for (int j = 0; j < image.height; j++)
     {
@@ -84,10 +84,78 @@ void FlipImageVertically(Image &image)
     image = emptyImage;
     image.saveImage("FlipV.png");
 }
-int GetChoice()
+void CropImage(Image image, int x, int y, int width, int hight)
 {
-    int choice;
-    vector<string> choices = {"1-GrayScale filter", "2-BlackAndWhite filter", "3-Flip Image Horizontally", "4-Flip Image Vertically"};
+    int emptyX = 0;
+    Image emptyImage(width, hight);
+    for (int i = x; i < x + width; i++)
+    { 
+        int emptyY = 0;
+        for (int j = y; j < y + hight; j++)
+        {
+            for (int k = 0; k < 3; k++)
+            {
+                emptyImage(emptyX, emptyY, k) = image(i, j, k);
+            }
+            emptyY++;
+        }
+        emptyX++;
+    }
+    emptyImage.saveImage("crop.png");
+}
+/*The multiplication factor f = (source bitmap width) / (destination bitmap width).
+In the picture above f = 8/3 = 2.6667
+f > 1 means reduction, f < 1 means magnification
+
+sx1 is the left postion of the red square, sx1 = f * x.
+Similar, sy1 = f * y is the top position.
+sx2 = sx1 + f.
+sy2 = sy1 + f.
+
+1. read source pixel[i,j]
+2. extract the red, green and blue values
+3. multiply these values by dx*dy and adding them up per color
+4. repeat 1..3 for all overlapping pixels
+5. pack the summed red, green and blue colors in a dword (32 bit integer)
+6. store this dword in destination bitmap [x,y]
+*/
+void brightness(Image image)
+{
+    bool inUse = true;
+    while (inUse)
+    {
+        float alphaScale;
+        int scale;
+        int response;
+        cout<<"1-Input increament scale\n"<<"2-Exit"<<endl;
+        cin>>response;
+        if(response == 1)
+        {
+            cin>>alphaScale;
+            //cin>>scale;
+            for(int i = 0; i < image.width; i++)
+            {
+            for(int j = 0; j < image.height; j++)
+            {
+                float Lumi = 0.2126*image(i,j,0) + 0.7152*image(i,j,1) + 0.0722*image(i,j,2); //Get luminance
+                image(i, j, 0) = image(i, j, 0) + (image(i, j, 0) * (alphaScale-1));
+                image(i, j, 1) = image(i, j, 1) + (image(i, j, 1) * (alphaScale-1));
+                image(i, j, 2) = image(i, j, 2) + (image(i, j, 2) * (alphaScale-1));
+            }
+            } 
+        }
+        else
+        {
+            inUse = false;
+        }
+    }
+    image.saveImage("brightness.png");
+}
+int GetChoice()                                                                 //make a save image|| 
+{                                                                               //                 ||
+    int choice;                                                                 //                \  /
+                                                                                //                 \/
+    vector<string> choices = {"1-Load a new image", "2-GrayScale filter", "3-BlackAndWhite filter", "4-Flip Image Horizontally", "5-Flip Image Vertically", "6-Crop image", "7-Adjust brightness", "8-Exit"};
     cout << "Choose" << endl;
     for (int i = 0; i < choices.size(); i++)
     {
@@ -98,26 +166,50 @@ int GetChoice()
 }
 int main()
 {
+    bool menuDisplayed = true;
     Image usedImage;
     string userInput;
     cout << "Input image" << endl;
     cin >> userInput;
     usedImage.loadNewImage(userInput);
-    switch (GetChoice())
+    while (menuDisplayed)
     {
-    case 1:
-        GrayScale(usedImage);
-        break;
-    case 2:
-        BlackAndWhite(usedImage);
-        break;
-    case 3:
-        FlipImageHorizontally(usedImage);
-        break;
-    case 4:
-        FlipImageVertically(usedImage);
-        break;
-    default:
-        break;
+        switch (GetChoice())
+        {
+        case 1:
+            cout<<"Input new image"<<endl;
+            cin >> userInput;
+            usedImage.loadNewImage(userInput);
+            break;
+        case 2:
+            GrayScale(usedImage);
+            break;
+        case 3:
+            BlackAndWhite(usedImage);
+            break;
+        case 4:
+            FlipImageHorizontally(usedImage);
+            break;
+        case 5:
+            FlipImageVertically(usedImage);
+            break;
+        case 6:
+            int x, y, width, hight;
+            cout << "Provide the starting point of x axis and y axis" << endl;
+            cin >> x >> y;
+            cout << "Provide the width and hight of the area you would like to crop"<<endl;
+            cin >> width >> hight;
+            CropImage(usedImage, x, y, width, hight);
+            break;
+        case 7:
+            brightness(usedImage);
+            break;
+        case 8:
+            menuDisplayed = false;
+            break;
+        default:
+            break;
+        }
     }
+    return 0;
 }
